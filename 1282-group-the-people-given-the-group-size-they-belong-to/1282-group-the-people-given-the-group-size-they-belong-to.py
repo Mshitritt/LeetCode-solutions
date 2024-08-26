bc3bec42ff02684909d1d3275e95905430ed8150
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-26 16:12:40 +0300

    Time: 43 ms (87.23%), Space: 12 MB (11.35%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -4,8 +4,10 @@ class Solution(object):
         :type groupSizes: List[int]
         :rtype: List[List[int]]
         """
+        # Pythonic solution 
         groupLs = []
         groupDic = {}
+        """
         for i in range(len(groupSizes)):
             gSize = groupSizes[i]   # key
             person = i              # value
@@ -15,7 +17,25 @@ class Solution(object):
                 ls = []
                 ls.append(person)
                 groupDic[gSize] = ls
-
+        """
+        """
+        Pythonic explanation:
+            dictionary.setdefault(key, default=None)
+            way to add a new pair of 'key: value' to dictionary or retrieve value of key
+            
+            value = my_dict.setdefault('c', 3)
+                1. if the pair 'c: 3' exist in my_dic --> method return the value of 'a' key 
+                2. if the pair 'c: 3' doesnt exist in my_dic --> method set the new pair and return the value of pair
+        
+            in our case:
+            the method check if exist empty list
+                if doesnt exist --> create a new one and set as value of size
+                if exist --> the method return the list 
+        """
+        for i, size in enumerate(groupSizes):
+            groupDic.setdefault(size, []).append(i)
+        
+        """
         for key in groupDic:
             ls_g = []
             size = key
@@ -29,7 +49,10 @@ class Solution(object):
                     ls_g.append(i)
                     size = key-1
             groupLs.append(ls_g)
-
+        """
+        for key, people in groupDic.items():
+            for i in range(0, len(people), key):
+                groupLs.append(people[i:i + key])
         return groupLs
 
 
