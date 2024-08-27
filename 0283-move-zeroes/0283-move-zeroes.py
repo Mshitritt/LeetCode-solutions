fa438fbf078d9c738c25e002c7bec4a2e3a6ec72
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-27 18:47:37 +0300

    Time: 117 ms (55.45%), Space: 12.9 MB (29.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,28 +7,18 @@ class Solution(object):
         
         if len(nums) <= 1:
             return None
-        
-        l = 0
-        r = l + 1
-        
-        while r <= len(nums)-1:
-            if nums[l] == 0 and nums[r] != 0:
-                # swap
-                temp = nums[l]
-                nums[l] = nums[r]
-                nums[r] = temp
 
-                l += 1
-                r += 1
-            elif nums[l] == 0 and nums[r] == 0:
-                r += 1
-            elif nums[l] != 0 and nums[r] == 0:
-                l += 1
-            else:
-                # nums[l] != 0 and nums[r] != 0
-                l += 1
-                r += 1
-                
-        return None 
+        non_zero_index = 0
 
+        # Move all non-zero elements to the front
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+
+        # Fill the rest with zeros
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0
+
+        return None
         
