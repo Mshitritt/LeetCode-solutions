54dd2d3ad2a9d7fd2b727f1658163994296ff797
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-28 12:01:34 +0300

    Time: 31 ms (94.1%), Space: 16.6 MB (11.44%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
@@ -3,33 +3,25 @@ class Solution:
         l = -1
         r = l + 1
         captures = 0
-        # edge case - empty, one element 
         while r < len(forts):
             tempCaptures = 0
             l += 1
             r = l + 1
             if forts[l] == 1:
-                # tempCaptures = 0
                 while r < len(forts) and forts[r] == 0:
                     tempCaptures += 1
                     r += 1
                 if r < len(forts) and forts[r] == -1:
                     captures = max(tempCaptures, captures)
-                # l = r
-                # r = l + 1
             elif forts[l] == -1:
-                # tempCaptures = 0
                 while r < len(forts) and forts[r] == 0:
                     tempCaptures += 1
                     r += 1
                 if r < len(forts) and forts[r] == 1:
                     captures = max(tempCaptures, captures)
-                # l = r
-                # r = l + 1
             else:
                 pass
-                # l += 1
-                # r = l + 1
+                
         return captures
 
 
