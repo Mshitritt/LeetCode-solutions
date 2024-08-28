a23e0b5556ec7ec9a06edc88e9df17b6dfb553fa
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-28 12:21:54 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
@@ -1,12 +1,13 @@
 class Solution:
     def captureForts(self, forts: List[int]) -> int:
-        l = -1
+        l = 0
         r = l + 1
         captures = 0
+        tempCaptures = 0
         while r < len(forts):
-            tempCaptures = 0
-            l += 1
-            r = l + 1
+            # tempCaptures = 0
+            # l += 1
+            # r = l + 1
             if forts[l] == 1:
                 while r < len(forts) and forts[r] == 0:
                     tempCaptures += 1
