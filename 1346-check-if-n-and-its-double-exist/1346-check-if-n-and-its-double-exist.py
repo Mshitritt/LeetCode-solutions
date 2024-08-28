53ab204385157f948fc80c905516098d4ca0923b
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-28 11:07:28 +0300

    Time: 52 ms (66.28%), Space: 16.6 MB (45.7%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,25 +1,10 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        if len(arr) < 2:
-            return False
-        arr.sort()
-        # 2, 3, 5, 6, 7, 8
-
-        for i in range(len(arr)):
-            target = arr[i]*2
-            low = 0
-            high = len(arr) - 1
-
-            while low <= high:
-                mid = (high + low) // 2
-
-                if arr[mid] == target and mid != i:
-                    return True
-                elif arr[mid] < target:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-
+        d = {}
+        for val in arr:
+            if d.get(val*2,0) or d.get(val/2,0): return True
+            d[val] = 1
+        
         return False
                     
 
