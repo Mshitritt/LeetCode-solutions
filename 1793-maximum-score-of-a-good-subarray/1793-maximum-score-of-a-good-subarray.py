1e1fab9fc37b3ca8206f10e094bbf5358b143378
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-30 12:37:20 +0300

    Time: 892 ms (67.2%), Space: 26.8 MB (95.7%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -1,25 +1,20 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
-        minimum = max_score = nums[k]
-        i = j = k
         n = len(nums)
+        left = k
+        right = k
+        ans = nums[k]
+        curr_min = nums[k]
+        
+        while left > 0 or right < n - 1:
+            if (nums[left - 1] if left else 0) < (nums[right + 1] if right < n - 1 else 0):
+                right += 1
+                curr_min = min(curr_min, nums[right])
+            else:
+                left -= 1
+                curr_min = min(curr_min, nums[left])
 
-        while i > 0 or j < n - 1:
-            # Check if we can expand the subarray
-            if i > 0 and j < n - 1:
-                # Expand the subarray towards the side with the larger neighbor
-                if nums[i - 1] >= nums[j + 1]:
-                    i -= 1
-                else:
-                    j += 1
-            elif i == 0 and j < n - 1:
-                j += 1
-            elif j == n - 1 and i > 0:
-                i -= 1
-
-            # Update the minimum and the maximum score
-            minimum = min(minimum, nums[i], nums[j])
-            max_score = max(max_score, minimum * (j - i + 1))
-            
-        return max_score
+            ans = max(ans, curr_min * (right - left + 1))
+        
+        return ans
                 
