76512c731544811234bf8ffb2b395360f2f8f10a
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-31 16:42:55 +0300

    Time: 68 ms (15.26%), Space: 17.6 MB (36.91%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        def isVowels(a):
+            a = a.capitalize()
+            if a == 'A' or a == 'E' or a == 'I' or a == 'O' or a == 'U':
+                return True
+            else:
+                return False
+
+        l = 0
+        r = len(s)-1
+        lst = list(s)
+        vowels = []
+        
+        
+        while l < r:
+            if isVowels(lst[l]) and isVowels(lst[r]):
+                lst[l], lst[r] = lst[r], lst[l]
+                r -= 1
+                l += 1
+            else:
+                if isVowels(lst[l]):
+                    r -= 1
+                else:
+                    l += 1
+        
+        return ''.join(lst)
