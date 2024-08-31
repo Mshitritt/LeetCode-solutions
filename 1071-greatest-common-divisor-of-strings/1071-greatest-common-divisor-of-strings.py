e0c531be87ddfd364dee662be259dcfb5e0c3b7c
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-08-31 11:52:05 +0300

    Time: 37 ms (53.39%), Space: 16.4 MB (97.63%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,31 +1,21 @@
+import math
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # find the shortest word
-        short_w = str1
-        long_w = str2
-        ls = len(str1)
-        ll = len(str2)
-        if ll < ls:
-            short_w = str2
-            long_w = str1
-            ls, ll = ll, ls
+        """
+        improve explanation:
+        the old version using I use in split method (O(n)) inside of loop --> total complexity O(n^2)
 
-        # find t dividor
-        t = short_w[:ls]
-        while t:
-            # return true if exist item that not equal to ''
-            srt_lst = short_w.split(t)
-            lng_lst = long_w.split(t)
-            exists_short = any(item != '' for item in srt_lst)
-            exists_long = any(item != '' for item in lng_lst)
-
-            if exists_short is False and exists_long is False:
-                return t
-            else:
-                ls -= 1
-                t = short_w[:ls]
-
-        return ''
+        in this version we using in regular gcd finction from math
+        """
+        # Check if str1 + str2 == str2 + str1; if not, there's no common divisor
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        # Find the greatest common divisor of the lengths of str1 and str2
+        gcd_len = math.gcd(len(str1), len(str2))
+        
+        # The largest common divisor string would be the prefix of length gcd_len of str1
+        return str1[:gcd_len]
 
 
         
