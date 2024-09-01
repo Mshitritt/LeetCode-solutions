936feb3c95f1789f184d4e0f606b70423a0addbe
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-01 14:44:36 +0300

    Time: 50 ms (93.9%), Space: 16.7 MB (52.47%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression-other_option.py
@@ -0,0 +1,56 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        # edge case if chars in size of 1
+        if n == 1:
+            return 1
+
+        # setting iterators
+        s_group = 0     # where the group start
+        e_group = 1     # where the group end
+        index = 0       # where to write the group letter and size
+        size_group = 1  # size of group
+        while e_group <= n:
+            # count how many variables have in group
+            if e_group < n and chars[s_group] == chars[e_group]:
+                e_group += 1
+                size_group += 1
+            else:
+                # write the compressed chars by algorithm conditions
+                # write the letter of group
+                chars[index] = chars[s_group]
+                index += 1
+                if size_group > 1:
+                    """
+                    # write the size of group
+                    size_lst = list(str(size_group))
+                    i_size = 0  # iterator of size list
+                    for i in range(index, index + len(size_lst)):
+                        chars[i] = size_lst[i_size]
+                        i_size += 1
+                        index += 1
+                    """
+                    # Write the count if greater than 1
+                    size_group = e_group - s_group
+                    if size_group > 1:
+                        for c in str(size_group):
+                            chars[index] = c
+                            index += 1
+
+                # setting iterators
+                s_group = e_group
+                e_group += 1
+                size_group = 1
+
+        # remove extra elements
+        for i in range(n-1, index-1, -1):
+            chars.pop()
+        """
+        # other option 
+        del chars[index:]
+        """
+
+        return len(chars)
+
+
+                    
