08a57669e85efb534938b35b7b40619a76d69011
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-02 17:37:03 +0300

    Time: 275 ms (36.52%), Space: 25.7 MB (86.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        nums = [2, 3, 4, 5]
+        ans = [60, 40, 30, 24]
+
+        prod_l = [1, 2, 6, 24]  # contain multiple all left element
+        prod_r = [60, 20, 5, 1]  # contain multiple all right element
+        ans = prod_l[i] * prod_r[i]
+        """
         n = len(nums)
-        ans = [0] * n
-        mul = 1
-        # search a zero in nums
-        zero_index = -1
-        for i in range(n):
-            if nums[i] != 0:
-                mul *= nums[i]
-            else:
-                if zero_index != -1:
-                    return ans
-                else:
-                    zero_index = i
+        ans = [0]*n
+        prod_l = [1]*n
+        prod_r = [1]*n
 
-        # exist only 1 zero in array
-        if zero_index != -1:
-            ans[zero_index] = mul
-            return ans
+        # fill prod_l
+        for i in range(1, n):
+            prod_l[i] = nums[i-1] * prod_l[i-1]
 
-        # calculate products
+        # fill prod_r
+        for i in range(n-2, -1, -1):
+            prod_r[i] = nums[i+1] * prod_r[i+1]
+
+        # calculate ans
         for i in range(n):
-            if i != zero_index:
-                ans[i] = mul // nums[i]
+            ans[i] = prod_l[i] * prod_r[i]
 
         return ans
