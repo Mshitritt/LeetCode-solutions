dbd5538aa87c513ccdd07d0139714985c42d7658
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-03 12:02:40 +0300

    Time: 229 ms (8.45%), Space: 16.6 MB (39.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,13 +4,18 @@ class Solution:
         # calculate number of changes
         l = 0
         r = 0
+        freq = {}
+        most_freq = 0
         while r < len(s):
             window = r - l
             # count frequency of each element
-            freq = dict(Counter(s[l:r + 1]))
+            if s[r] in freq:
+                freq[s[r]] += 1
+                most_freq = max(most_freq, freq[s[r]])
+            else:
+                freq[s[r]] = 1
 
             # find the most frequency char
-            most_freq = 0
             for key in freq:
                 most_freq = max(most_freq, freq[key])
 
@@ -18,9 +23,11 @@ class Solution:
             curr_changes = window - most_freq + 1
 
             if curr_changes > k:
+                freq[s[l]] -= 1
                 l += 1
                 r += 1
             else:
                 r += 1
 
         return r - l
+
