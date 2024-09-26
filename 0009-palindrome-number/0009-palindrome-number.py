eb593e3ca37fcc866320d3a688bd3d0712e56d91
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-26 18:43:51 +0300

    Time: 53 ms (60.18%), Space: 16.8 MB (7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,15 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        # Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
-        x_str = str(x)
-        left = 0
-        right = len(x_str)-1
-        while left < right:
-            if x_str[left] != x_str[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
+        reversed_half = 0
+        while x > reversed_half:
+            # Build the reversed number one digit at a time
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # If the original number is a palindrome, the first half should equal the second half
+        # For odd-length numbers, we can ignore the middle digit by reversed_half // 10
+        return x == reversed_half or x == reversed_half // 10
 
