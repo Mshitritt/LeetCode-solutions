d8464bc762304fcd24e3b1fbae799d6fd92d8621
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-27 10:31:16 +0300

    Time: 42 ms (92.34%), Space: 17.3 MB (41.21%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,26 +3,15 @@ class Solution:
         low = 0
         high = len(nums)-1
         mid = 0
-        # if target place is first
-        if target < nums[low]:
-            return low 
-        # if target place is last
-        if target > nums[high]:
-            return high + 1
-
-        if target == nums[high]:
-            return high
-
-        while low < high:
-            mid = (low + high) // 2
+        while low <= high:
+            mid = (low + high)//2
             if nums[mid] == target:
                 return mid
-            elif nums[low] < target < nums[high] and low+1 == high:
-                return low + 1
             elif nums[mid] < target:
                 low = mid + 1
             else:
                 high = mid - 1
-        
-        
+
+        if nums[mid] < target:
+            return mid + 1
         return mid
