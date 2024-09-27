ef59c74a3682a29aee7b61da481e59e9a688f236
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-27 12:22:25 +0300

    Time: 43 ms (9.18%), Space: 16.6 MB (11.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,25 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stuck = [] 
-        i = 0
-        while i < len(s):
-            if s[i] == '(' or s[i] == '{' or s[i] == '[':
-                stuck.append(s[i])
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map:
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
             else:
-                if len(stuck) == 0:
-                    return False
-                # stuck not empty
-                openBrace = stuck.pop()
-                if openBrace == '(' and s[i] == ')':
-                    pass
-                elif openBrace == '{' and s[i] == '}':
-                    pass
-                elif openBrace == '[' and s[i] == ']':
-                    pass
-                else:
-                    return False
-            i += 1
-        return True
+                return False
+
+        return not stack
+
 
 
             
