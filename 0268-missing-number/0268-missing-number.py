c66165fe3ba896fa72282c5d6848bf020c22156c
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-27 10:58:23 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        numsSet = set(nums)
+        # if missing is in middl 
+        for i in range(len(nums)):
+            if nums[i] not in numsSet:
+                return i
+
+        # if missing in the end 
+        return len(nums)
+         
