a8619a387b63d4566ba2b36f6ad420d99d2c80a2
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-09-29 16:13:10 +0300

    Time: 568 ms (13.64%), Space: 31.1 MB (16.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,15 +3,12 @@ class Solution:
         n = len(nums)
         if n == 0:
             return 0
-        if n == 1:
-            return nums[0]
+        
 
         max_total = nums[0]
         curr_sum = nums[0]
 
         for i in range(1, n):
             curr_sum = max(nums[i], curr_sum+nums[i])
-            
-            
             max_total = max(curr_sum, max_total)
         return max_total 
