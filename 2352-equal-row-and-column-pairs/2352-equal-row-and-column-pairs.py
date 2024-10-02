beb2343fbe7ebfc59afa58bd4eb21fd36ea2347d
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-10-02 12:12:41 +0300

    Time: 455 ms (37.66%), Space: 21.9 MB (9.9%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -12,12 +12,7 @@ class Solution:
             
             mapp.setdefault(key, 0)
             mapp[key] += 1
-            """
-            if mapp.setdefault(key, 0) == 0:
-                mapp[key] = 1
-            else:
-                mapp[key] += 1
-            """
+            
         # column counting
         for r in range(len(grid)):
             key = ""
