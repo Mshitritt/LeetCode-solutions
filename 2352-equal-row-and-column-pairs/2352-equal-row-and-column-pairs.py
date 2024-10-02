9e1cd3b4a0a3d2a24bf21851ecd2674f31c6143e
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-10-02 12:06:55 +0300

    Time: 460 ms (36.65%), Space: 21.5 MB (57.46%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -9,14 +9,15 @@ class Solution:
             for c in range(len(grid[r])):
                 key += str(grid[r][c])+','
 
-            """
-            mapp.setdefault(key, 0) == 0:
+            
+            mapp.setdefault(key, 0)
             mapp[key] += 1
             """
             if mapp.setdefault(key, 0) == 0:
                 mapp[key] = 1
             else:
                 mapp[key] += 1
+            """
         # column counting
         for r in range(len(grid)):
             key = ""
