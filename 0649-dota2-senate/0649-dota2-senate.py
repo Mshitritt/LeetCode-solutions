23cd1048925add2f19dc53233a7bb3dcc651de28
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-10-31 11:17:38 +0200

    Time: 63 ms (28.87%), Space: 17.3 MB (11.1%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,40 +1,31 @@
 class Solution:
+    
     def predictPartyVictory(self, senate: str) -> str:
-        party = list(senate)
-        n = len(party)
-        bans = [1]*n    # 1- notBan, 0- Ban
-        d_count = 0
-        r_count = 0
-        for r in party:
-            if r == "R":
-                r_count += 1
+        n = len(senate)
+        d_queue = []
+        r_queue = []
+        for i in range(n):
+            if senate[i] == "R":
+                r_queue.append(i)
             else:
-                d_count += 1
-        curr = 0
-        nxt = 1
-        while d_count != 0 and r_count != 0:
-            if bans[curr] == 0:
-                curr += 1
-                if curr >= n:
-                    curr = curr %n
-            if party[nxt] == party[curr] or bans[nxt] == 0:
-                nxt += 1
-                if nxt >= n:
-                    nxt = nxt % n
+                d_queue.append(i)
+        while d_queue and r_queue:
+            d = d_queue.pop(0)
+            r = r_queue.pop(0)
+            if d < r:
+                # d won
+                d_queue.append(d+n)
             else:
-                bans[nxt] = 0
-                if party[nxt] == "R":
-                    r_count -= 1
-                else:
-                    d_count -= 1
-                curr += 1
-                nxt = curr + 1
-                if curr >= n:
-                    curr = curr % n
-                if nxt >= n:
-                    nxt = nxt % n
-        if d_count == 0:
-            return "Radiant"
-        else:
+                r_queue.append(r+n)
+                
+
+
+
+
+        if d_queue:
             return "Dire"
-            
+        else:
+            return "Radiant"
+
+
+                
