9cae736b56bfd24f4fb2ace4f0dda0a6923d4341
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-10-31 11:19:12 +0200

    Time: 62 ms (29.85%), Space: 17 MB (27.73%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate-pythonList.py
@@ -0,0 +1,27 @@
+class Solution:
+    
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        d_queue = []
+        r_queue = []
+        for i in range(n):
+            if senate[i] == "R":
+                r_queue.append(i)
+            else:
+                d_queue.append(i)
+        while d_queue and r_queue:
+            d = d_queue.pop(0)
+            r = r_queue.pop(0)
+            if d < r:
+                # d won
+                d_queue.append(d+n)
+            else:
+                r_queue.append(r+n)
+                
+        if d_queue:
+            return "Dire"
+        else:
+            return "Radiant"
+
+
+                
