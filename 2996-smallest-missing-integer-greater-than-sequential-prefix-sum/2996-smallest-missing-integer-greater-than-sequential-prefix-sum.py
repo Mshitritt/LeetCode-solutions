f0c4b1c5f246de59a7bc7829a3d8ce521059fb9c
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-11-17 15:30:32 +0200

    Time: 0 ms (100%), Space: 16.6 MB (25.27%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -1,48 +1,21 @@
-class Solution(object):
-    def missingInteger(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        """
-        Missing edge case:
-        nums1 = [1, 2, 3, 2, 5] --> output 6 is correct
-        nums2 = [46, 1, 2, 3] --> output 46 is wrong since nums2 includes 46
-        so you need to check if the sum of sequencd numers not include in nums
-        """
-        seqSum = nums[0]
-        endSeqIndex = 0
-        forbidden = []
-        for i in range(1, len(nums)):
-            # find the sequence numbers 
-            if nums[i] == nums[i-1] + 1:
-                seqSum += nums[i]
-            else:
-                # we have the sequence numbers 
-                endSeqIndex = i
-                break
-
-        # case of 1 element in sequence numbers 
-        numsRest = nums[endSeqIndex:]
-        if endSeqIndex == 1:
-            seqSum += 1
-            
-        while numsRest:
-            m = min(numsRest)
-            M = max(numsRest)
-            if seqSum > M:
-                return seqSum
-            elif seqSum == m:
-                seqSum += 1
-                numsRest.remove(m)
-            else:
-                numsRest.remove(m)
-
-
-        return seqSum
-                
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        prefix = nums[0]
+        prev = nums[0]
+        i = 1
         
+        while i < len(nums) and nums[i] == prev+1:
+            prefix += nums[i]
+            prev = nums[i]
+            i += 1
+        
+        
+        unique = set(nums)
+
+        while True:
+            if prefix not in unique:
+                return prefix
+            prefix += 1
             
 
-        
-        
+       
