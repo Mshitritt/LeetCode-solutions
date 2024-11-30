074977d58a1d04834a2ca1fc04b07c2e5d4a97a2
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-11-30 09:17:22 +0200

    Time: 536 ms (28.36%), Space: 17.9 MB (55%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0 for _ in range(amount+1)] for _ in range(2)]
+        
+        for i in range(2):
+            dp[i][0] = 1
+        
+        for a in range(amount+1):
+            if a-coins[0] >= 0:
+                dp[0][a] += dp[0][a-coins[0]]
+        
+
+        for c in range(1, len(coins)):
+            dp[0][0] = 1
+            dp[1][0] = 1
+            for a in range(1, amount+1):
+                if a-coins[c] >= 0:
+                    dp[1][a] += dp[0][a] + dp[1][a-coins[c]]
+                else:
+                    dp[1][a] += dp[0][a]
+
+            # swap lines 
+            for i in range(amount+1):
+                dp[0][i], dp[1][i] = dp[1][i], 0
+            
+        
+        return dp[0][amount]
+
+            
+
+        
+        for r in dp:
+            print(r)
+        
+        return dp[-1][amount]
+                
+
+            
+
+                
