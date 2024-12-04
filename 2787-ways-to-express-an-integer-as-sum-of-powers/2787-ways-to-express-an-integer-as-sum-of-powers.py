63bce36888827a8447cfa9693d454d08a6cdccda
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-12-04 14:35:47 +0200

    Time: 1134 ms (51.65%), Space: 17.5 MB (55%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.py
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.py
@@ -9,21 +9,25 @@ class Solution:
         maxBase -= 1  # Adjust to the largest valid base
 
         # Initialize DP table
-        dp = [[0] * (maxBase + 1) for _ in range(n + 1)]
-        for base in range(maxBase + 1):
-            dp[0][base] = 1  # Base case: 1 way to make sum 0
+        dp = [[0] * (2) for _ in range(n + 1)]
+        dp[0][0] = 1
+        dp[0][1] = 1
 
         # Fill DP table
         for base in range(1, maxBase + 1):
             power = base**x
             for total in range(n + 1):
                 # Exclude current base
-                dp[total][base] = dp[total][base - 1]
+                dp[total][1] = dp[total][0]
                 # Include current base if possible
                 if total >= power:
-                    dp[total][base] = (dp[total][base] + dp[total - power][base - 1]) % MOD
+                    dp[total][1] = (dp[total][1] + dp[total - power][0]) % MOD
+            
+            # prepare dp for next iteration 
+            for i in range(n+1):
+                dp[i][0] = dp[i][1]
 
         for r in dp:
             print(r)
-        return dp[n][maxBase]
+        return dp[n][0]
 
