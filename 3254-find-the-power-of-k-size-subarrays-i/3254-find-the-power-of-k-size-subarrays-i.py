9ea7b9682b484be91743c32d660c4c7568b74a75
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-12-06 13:37:54 +0200

    Time: 7 ms (63.44%), Space: 17.7 MB (5.1%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -2,16 +2,32 @@ class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         res = [-1]*(len(nums)-k+1)
         q = deque()
-
+        maxVal = -float('inf')
+        maxIdx = None
+        counter = 0
         for r in range(len(nums)):
             curr = nums[r]
+            """
             if q and curr != q[-1]+1:
                 q.clear()
             q.append(curr)
+            """
+            if maxIdx != None and curr != maxVal+1:
+                maxVal = -float('inf')
+                maxIdx = None
+                counter = 0
+            maxVal = nums[r]
+            maxIdx = r
+            counter += 1
 
+            if counter == k:
+                res[r-k+1] = maxVal
+                counter -= 1
+            """
             if len(q) == k:
                 res[r-k+1] = q[-1]
                 q.popleft()
+            """
         return res
 
             
