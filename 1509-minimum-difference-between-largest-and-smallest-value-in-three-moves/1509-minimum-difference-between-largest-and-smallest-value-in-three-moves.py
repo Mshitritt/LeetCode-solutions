47c740993f969314a24bdbc0693117e9df65591e
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-12-17 11:20:22 +0200

    Time: 19 ms (87.37%), Space: 27.8 MB (6.9%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -3,11 +3,28 @@ class Solution:
         if len(nums) < 5:
             return 0
 
-        nums = sorted(nums)
+        
         res = float('inf')
-        op1 = nums[-4] - nums[0]    # 3 larget
-        op2 = nums[-1] - nums[3]    # 3 smallest
-        op3 = nums[-3] - nums[1]    # 2 largest, 1 smallest
-        op4 = nums[-2] - nums[2]    # 1 largest, 2 smallest
-        return min(op1, op2, op3, op4)
+        maxHeap = []    # store maximum elements from **small -> large**
+        minHeap = []    # store minimum elements from **large -> small**
+        
+        for num in nums:
+            if len(maxHeap) < 4:
+                heapq.heappush(maxHeap, num)
+                heapq.heappush(minHeap, -num)
+            else:
+                if num >= maxHeap[0]:
+                    heapq.heappushpop(maxHeap, num)
+                if -num >= minHeap[0]:
+                    heapq.heappushpop(minHeap, -num)
+        
+        minHeap = sorted([-x for x in minHeap])
+        maxHeap = sorted([x for x in maxHeap], reverse=True)
+        for i in range(4):
+            res = min(res, maxHeap[3-i] - minHeap[i]) 
+        # op1 = nums[-4] - nums[0]    # 3 larget
+        # op3 = nums[-3] - nums[1]    # 2 largest, 1 smallest
+        # op4 = nums[-2] - nums[2]    # 1 largest, 2 smallest
+        # op2 = nums[-1] - nums[3]    # 3 smallest
+        return res
         
