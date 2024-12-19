5c1181886fdf88602d36489d61006934b9c8e17f
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-12-19 15:54:40 +0200

    Time: 3 ms (85.46%), Space: 19.9 MB (17.22%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: x[0])  # Sort by start time
-        q = deque()  # stores end times in sorted order
+        min_heap = []
         max_room = 0
         
         for s, e in intervals:
             # Remove meetings that have ended
-            while q and q[0] <= s:
-                q.popleft()
+            while min_heap and min_heap[0] <= s:
+                heapq.heappop(min_heap)
             
             # Insert end time in sorted order
-            i = 0
-            while i < len(q) and q[i] < e:
-                i += 1
-            q.insert(i, e)
+            heapq.heappush(min_heap, e)
             
-            max_room = max(max_room, len(q))
+            max_room = max(max_room, len(min_heap))
             
         return max_room
                 
