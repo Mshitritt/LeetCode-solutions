c5557cf2c0b4133669d3b91df28e373f3181333a
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-12-20 16:58:17 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -0,0 +1,53 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        slots1.sort()
+        slots2.sort()
+        i1, n1 = 0, len(slots1)
+        i2, n2 = 0, len(slots2)
+        res = []
+
+        
+        while i1 < n1 and i2 < n2:
+            s1, e1 = slots1[i1]
+            s2, e2 = slots2[i2]
+
+            # case no intersection 
+            while (i1 < n1 and e1 <= s2) or e2 <= s1:
+                i1 += 1
+                if i1 == n1:
+                    break
+                else:
+                    s1, e1 = slots1[i1]
+            
+            while (i2 < n2 and e2 <= s1) or e1 <= s2:
+                i2 += 1
+                if i2 == n2:
+                    break
+                else:
+                    s2, e2 = slots2[i2]
+            
+            if i1 == n1 or i2 == n2:
+                return res
+            
+            # case have intersection 
+            s1, e1 = slots1[i1]
+            s2, e2 = slots2[i2]
+
+            
+            newS = max(s1, s2)
+            newE = min(e1, e2)
+
+            if newE - newS >= duration:
+                # have intersection 
+                return [newS, newS+duration]
+            else:
+                # edge case - intersection slot small then duretion 
+                if e1 < e2:
+                    i1 += 1
+                else:
+                    i2 += 1
+
+
+        return []
+            
+            
