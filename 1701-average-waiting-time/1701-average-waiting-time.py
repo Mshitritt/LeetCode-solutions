b58bc99fbb22c4e1c894aa31dc630e8f602d09c5
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2024-12-22 16:27:19 +0200

    Time: 26 ms (51.18%), Space: 49.5 MB (5.19%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,14 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        res = 0
+        wait = 0
+        finish = customers[0][0]
+        prev = customers[0][1]
+        for arr, t in customers:
+            wait = max(0, finish-arr)
+            res += wait + t
+            finish = arr + t + wait
+
+        return res/len(customers)
+
+            
