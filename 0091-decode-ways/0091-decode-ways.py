5b7d31a0435ad7a4f6a6e7ba7ccca4b2f0dc8a2d
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2025-01-01 09:50:22 +0200

    Time: 0 ms (100%), Space: 17.8 MB (11.49%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,14 +4,18 @@ class Solution:
             return 0 
         dp = [0]*(len(s)+1)
         # base case
-        dp[0], dp[1] = 1, 1
-
+        prev_2, prev_1 = 1, 1 # 1-last index, 2 - two last indices
+        res = 0
         for i in range(2, len(s)+1):
             l = s[i-1]
             if l != '0':
-                dp[i] = dp[i-1]
+                # dp[i] = dp[i-1]
+                res = prev_1
             if 10 <= int(s[i-2]+l) <= 26: 
-                dp[i] += dp[i-2]
+                # dp[i] += dp[i-2]
+                res += prev_2
+            prev_2, prev_1 = prev_1, res
+            res = 0
         
-        return dp[len(s)]
+        return prev_1
             
