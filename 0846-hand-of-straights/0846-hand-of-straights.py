74c4335756c8e4089f1592486971e5a01375ce72
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2025-01-13 18:37:38 +0200

    Time: 35 ms (59.65%), Space: 19.3 MB (14.04%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -4,17 +4,16 @@ class Solution:
         freq = Counter(hand)
         cards = sorted(list(freq.keys()))
 
-        groups = 0
+        
         for c in cards:
-            if c not in freq:
-                continue
-            for i in range(c, c + groupSize):
-                if i not in freq:
-                    return False
-                freq[i] -= 1
-                if not freq[i]:
-                    del freq[i]
-            groups += 1
+            while freq[c]:
+                for i in range(c, c + groupSize):
+                    if i not in freq:
+                        return False
+                    freq[i] -= 1
+                    if freq[i] < 0:
+                        return False
+            
         
         return True
                 
