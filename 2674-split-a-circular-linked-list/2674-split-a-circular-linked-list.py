929ef2c0ed1b9bce27fb51df8b1fe008c1d47006
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2025-02-13 09:01:11 +0200

    Time: 383 ms (94.34%), Space: 43 MB (80.19%) - LeetHub

--- 2674-split-a-circular-linked-list/2674-split-a-circular-linked-list.py
+++ 2674-split-a-circular-linked-list/2674-split-a-circular-linked-list.py
@@ -4,41 +4,70 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def splitCircularLinkedList(self, list: Optional[ListNode]) -> List[Optional[ListNode]]:
-        slow, fast = list, list
-        size = 0
-        head = list
-
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
+    def splitCircularLinkedList(self, node: Optional[ListNode]) -> List[Optional[ListNode]]:
+        slow = fast = node
+        while fast.next != node:
+            if fast.next.next == node:
                 fast = fast.next
-                while slow != fast:
-                    size += 1
+            else:
+                fast = fast.next.next
+                slow = slow.next
+        
+        fast.next = slow.next
+        slow.next = node
+
+        return (node, fast.next)
+        """
+        My solution but more clearly 
+        -----------------------------------
+        -- you can do with less logic -- 
+        -- Check WHY is working ??? 
+
+        def splitCircularLinkedList(self, node: Optional[ListNode]) -> List[Optional[ListNode]]:
+            slow = fast = node
+            while fast.next != node:
+                if fast.next.next == node:
                     fast = fast.next
-                break
-        h1 = math.ceil(size // 2)
+                else:
+                    fast = fast.next.next
+                    slow = slow.next
+            
+            fast.next = slow.next
+            slow.next = node
+
+            return (node, fast.next)
+        -----------------------------------------
+        """
+        """
+        if not list or not list.next:
+            return [list, None]
+
+        # Step 1: Find the length of the circular linked list
+        length = 1
+        current = list
+        while current.next != list:
+            current = current.next
+            length += 1
+        
+        # Step 2: Use slow and fast pointer to find the middle
+        mid = (length + 1) // 2  # This ensures ceil(length / 2)
+        prev = None
         slow = list
-        while h1:
+        for _ in range(mid):  
+            prev = slow
             slow = slow.next
-            h1 -= 1
-        fast = slow.next
-        h2Head = slow.next
-        slow.next = list
-        
-        while fast.next != list:
-            fast = fast.next
-        fast.next = h2Head
 
-        return [slow.next, h2Head]
-        
-        
+        # Step 3: Break the circular list into two parts
+        prev.next = list  # First half remains circular
 
-        
-        # first half head ---> slow
-        return []
+        # Find the last node of the second half
+        last = slow
+        while last.next != list:
+            last = last.next
+        last.next = slow  # Second half remains circular
 
+        return [list, slow]
+        """
         
 
         
