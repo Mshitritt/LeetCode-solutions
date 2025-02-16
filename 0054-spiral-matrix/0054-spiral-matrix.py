052e48acf35c85b84f7d034f1601cbfc1e927300
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2025-02-16 12:25:35 +0200

    Time: 0 ms (100%), Space: 17.6 MB (98.18%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,57 +1,36 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
-        
-        """
-        1   2   3   4
-        5   6   7   8   
-        9   10  11  12
-        13  14  15  16
-
-        1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
-        """
         res = []
-        rows = len(matrix)
-        cols = len(matrix[0])
-        r = 0
-        c = 0
+        if not matrix:
+            return res
 
+        l, r = 0, len(matrix[0])
+        t, b = 0, len(matrix)
+        while l < r and t < b:
+            # top row
+            for i in range(l, r):
+                res.append(matrix[t][i])
+            t += 1
 
-        while True:
-            # right move
-            while c < cols and matrix[r][c] != -200:
-                res.append(matrix[r][c])
-                matrix[r][c] = -200
-                c += 1
-
-            r += 1
-            c -= 1
-            # down move
-            while r < rows and matrix[r][c] != -200:
-                res.append(matrix[r][c])
-                matrix[r][c] = -200
-                r += 1
-
-            c -= 1
+            # right column
+            for i in range(t, b):
+                res.append(matrix[i][r-1])
             r -= 1
-            # left move
-            while c >= 0 and matrix[r][c] != -200:
-                res.append(matrix[r][c])
-                matrix[r][c] = -200
-                c -= 1
 
-            r -= 1
-            c += 1
-            # up move
-            while r > 0 and matrix[r][c] != -200:
-                res.append(matrix[r][c])
-                matrix[r][c] = -200
-                r -= 1
+            # Edge case 
+            if t >= b or l >= r:
+                break
 
-            c += 1
-            r += 1
-            if r >= rows or c >= cols or matrix[r][c] == -200:
-                return res
-            
+            # buttom row
+            for i in range(r-1, l-1, -1):
+                res.append(matrix[b-1][i])
+            b -= 1
 
+            # left column
+            for i in range(b-1, t-1, -1):
+                res.append(matrix[i][l])
+            l += 1
 
+        return res
+        
+   
