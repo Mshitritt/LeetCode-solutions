fde1665084c7d3f0275bf9736bf3ef1c2d05c0dc
Mshitritt <90529221+Mshitritt@users.noreply.github.com>
2025-02-19 15:55:31 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return [[""]]
+        
+        freq = defaultdict(list)
+        for s in strs:
+            key = 0
+            for l in s:
+                key += ord(l)
+            freq[key].append(s)
+        
+        res = []
+        for _, s_list in freq.items():
+            res.append(s_list)
+        return res
