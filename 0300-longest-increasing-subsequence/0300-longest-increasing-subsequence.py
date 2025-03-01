be794279b9de0400c6bd175112e2eb52c1e6def9
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-01 14:08:30 +0200

    Time: 3 ms (96.16%), Space: 18.2 MB (38.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,9 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1]*len(nums)
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j]+1)
-        return max(dp)
+        sub = []
+        for num in nums:
+            i = bisect_left(sub, num)
 
+            # If num is greater than any element in sub
+            if i == len(sub):
+                sub.append(num)
+            
+            # Otherwise, replace the first element in sub greater than or equal to num
+            else:
+                sub[i] = num
+        
+        return len(sub)
