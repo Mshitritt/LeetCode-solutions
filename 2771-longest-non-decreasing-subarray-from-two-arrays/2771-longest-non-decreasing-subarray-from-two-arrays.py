7253db7cde04b3cfc982501a30b7f58b46957fbd
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-11 16:22:33 +0200

    Time: 144 ms (86.33%), Space: 33.5 MB (86.33%) - LeetHub

--- 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.py
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.py
@@ -1,28 +1,38 @@
 class Solution:
     def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
-        # dp[0] for nums 1
-        # dp[1] for nums 2
-        dp = [[1, 1] for _ in range(len(nums1))]    
+
+        n = len(nums1)
+        if n == 1:
+            return 1
+        curr_1, curr_2 = 1, 1
+        
         res = 1
 
-        for i in range(1, len(nums1)):
+        for i in range(1, n):
             n1, n2 = nums1[i], nums2[i]
+            temp_1, temp_2 = 1, 1
 
-            # end with nums 1 
+            # Ending nums1
             if n1 >= nums1[i-1]:
-                dp[i][0] = max(dp[i][0], dp[i-1][0]+1)
+                temp_1 = curr_1 + 1
             if n1 >= nums2[i-1]:
-                dp[i][0] = max(dp[i][0], dp[i-1][1]+1)
+                temp_1 = max(temp_1, curr_2 + 1)
+            
+            
 
-            # end with nums 2
+            # Ending nums2
             if n2 >= nums2[i-1]:
-                dp[i][1] = max(dp[i][1], dp[i-1][1]+1)
+                temp_2 = curr_2 + 1
             if n2 >= nums1[i-1]:
-                dp[i][1] = max(dp[i][1], dp[i-1][0]+1)
+                temp_2 = max(temp_2, curr_1 + 1)
 
-            res = max(res, dp[i][0], dp[i][1])
+            # Update result
+            curr_1, curr_2 = temp_1, temp_2
+            res = max(res, curr_1, curr_2)
         
         return res
+            
+        
 
 
 
