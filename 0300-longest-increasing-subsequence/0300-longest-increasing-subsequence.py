cb8849a8cb65b87e2e7fba5e4da1b18f9c8aac4d
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-11 14:01:01 +0200

    Time: 6267 ms (5%), Space: 66.8 MB (5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,15 +1,28 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = []
-        for num in nums:
-            i = bisect_left(sub, num)
+        n = len(nums)
+        if n == 0:
+            return 0
 
-            # If num is greater than any element in sub
-            if i == len(sub):
-                sub.append(num)
-            
-            # Otherwise, replace the first element in sub greater than or equal to num
-            else:
-                sub[i] = num
+        dp = [[0]*n for _ in range(n+1)]
+        result = 1
+
+        for i in range(1, n+1):
+            for j in range(i):
+                if j == 0:
+                    # Starting a new subsequence explicitly at i-1
+                    dp[i][i-1] = max(dp[i][i-1], 1)
+                else:
+                    if nums[i-1] > nums[j-1]:
+                        # Explicitly extending previous subsequence ending at j-1
+                        dp[i][i-1] = max(dp[i][i-1], dp[j][j-1] + 1)
+
+                # Explicitly carry forward previous state if not picking current element
+                dp[i][j] = max(dp[i][j], dp[i-1][j])
+
+        # Explicitly finding global optimal
+        result = 0
+        for j in range(len(nums)):
+            result = max(result, dp[n][j])
         
-        return len(sub)
+        return result
