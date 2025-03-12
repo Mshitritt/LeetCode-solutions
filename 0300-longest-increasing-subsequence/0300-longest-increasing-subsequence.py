cedb90e921d75234c2adf8062fde8492083ed5cf
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-12 10:25:30 +0200

    Time: 6366 ms (5.01%), Space: 68.1 MB (5.07%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,28 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 0:
-            return 0
-
-        dp = [[0]*n for _ in range(n+1)]
-        result = 1
-
-        for i in range(1, n+1):
-            for j in range(i):
-                if j == 0:
-                    # Starting a new subsequence explicitly at i-1
-                    dp[i][i-1] = max(dp[i][i-1], 1)
-                else:
-                    if nums[i-1] > nums[j-1]:
-                        # Explicitly extending previous subsequence ending at j-1
-                        dp[i][i-1] = max(dp[i][i-1], dp[j][j-1] + 1)
-
-                # Explicitly carry forward previous state if not picking current element
-                dp[i][j] = max(dp[i][j], dp[i-1][j])
-
-        # Explicitly finding global optimal
-        result = 0
-        for j in range(len(nums)):
-            result = max(result, dp[n][j])
+        # Create a sorted copy of nums (removing duplicates)
+        sorted_nums = sorted(set(nums))
         
-        return result
+        m, n = len(nums), len(sorted_nums)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if nums[i-1] == sorted_nums[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        # Answer is in dp[m][n]
+        return dp[m][n]
