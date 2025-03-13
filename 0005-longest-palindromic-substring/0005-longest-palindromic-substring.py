23aab668909227a425f55d04dd69b6d91ba2d632
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-13 13:41:38 +0200

    Time: 2005 ms (26.35%), Space: 25.5 MB (10.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,31 +1,66 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # LIS 
+        rev = s[::-1]
+        res = [0, 0]    # l, r
         n = len(s)
-        dp = [[0 for c in range(n)] for r in range(n)]
-        start = 0
-        end = 0
+        
+        dp = [[0]*n for _ in range(n)]
 
-        # main diagonal
+        # intionlaiz
+        # base case - single char
         for i in range(n):
             dp[i][i] = 1
-
-        # Palindrome in length 2
-        for r in range(1, n):
-            c = r - 1
-            if s[r] == s[c]:
-                dp[r][c] = 1
-                start = c
-                end = r
-
-        # Palindrome in length grater than 2
-        for l in range(3, n+1):
-            for c in range(n - l + 1):
-                r = c + l - 1
-
-                if s[r] == s[c] and dp[r-1][c+1]:
-                    dp[r][c] = 1
-                    start = c
-                    end = r
-
-        return ''.join(s[start: end+1])
         
+        # base case - 2 chars
+        for i in range(n-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = 1
+                res = [i, i+1]
+
+        # fill
+        for diff in range(3, n+1):
+            for l in range(n-diff+1):
+                r = l + diff -1
+                if s[l] == s[r]:
+                    dp[l][r] = dp[l+1][r-1]
+                    if dp[l][r]:
+                        res = [l, r]
+
+        
+        l, r = res
+        return s[l:r+1]
+            
+
+
+
+        
+        """
+        # LCS -- > find SUBSEQUENCE, not SUBSTRING --> Wrong
+        rev = s[::-1]
+        n = len(s)
+        dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if s[i-1] == rev[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        # recover solution
+        res = []
+        i, j = n, n
+        while i > 0 and j > 0 and dp[i][j]:
+            if s[i-1] != rev[j-1]:
+                if dp[i-1][j] > dp[i][j-1]:
+                    i -= 1
+                else:
+                    j -= 1
+            else:
+                res.append(s[i-1])
+                i -= 1
+                j -= 1
+        
+        return "".join(res)
+        """
