06a04a983502facdf9b0f8a59b487e2ca2bbf5e7
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-13 17:04:04 +0200

    Time: 567 ms (59.87%), Space: 17.9 MB (92.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,35 +1,17 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        if total % 2 == 1:
+        if total % 2:
             return False
-        need = total // 2
-        dp = [False]*(need+1)
-        
-        dp[0] = True
-        for curr in nums:
-            for j in range(need, curr - 1, -1):
-                dp[j] = dp[j] or dp[j - curr]
-        return dp[need]
 
-        """
-        total = sum(nums)
-        if total % 2 == 1:
-            return False
-        need = total // 2
-        dp = [[False for _ in range(need+1)]for _ in range(len(nums)+1)]
-        
-        dp[0][0] = True
-        for r in range(1, len(nums)+1):
-            curr = nums[r-1]
-            for c in range(need+1):
+        half = total // 2
+        dp = [False] * (half + 1)
+        dp[0] = True
+        for i in range(len(nums)):
+            num = nums[i]
+            for h in range(half, num-1, -1):
+                dp[h] = dp[h-num] or dp[h]
                 
-                if curr <= c: 
-                    dp[r][c] = dp[r-1][c] or dp[r-1][c-curr]
-                else: 
-                    dp[r][c] = dp[r-1][c]
-        for r in dp:
-            print(r)
-        return dp[len(nums)][need]
-        """     
+        return dp[half]
+
         
