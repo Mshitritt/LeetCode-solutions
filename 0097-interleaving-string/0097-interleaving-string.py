6484b498bb4d5196aa9c2cb2c87dbd3d958d8f93
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-17 14:38:30 +0200

    Time: 58 ms (7.47%), Space: 17.8 MB (79.97%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,37 +2,42 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if not s1 and not s2 and not s3:
             return True
-        
-        if len(s1) + len(s2) != len(s3):
+        n1, n2 = len(s1), len(s2)
+        if n1 + n2 != len(s3):
             return False
         
-        memo = {}
-        # rows = s1, cols = s2
+        dp = [[False for _ in range(n2+1)] for _ in range(n1 + 1)]
+        
+        for i1 in range(n1 + 1):
+            for i2 in range(n2 + 1):
+                if i1 == i2 == 0:
+                    dp[0][0] = True
+                elif i1 == 0:
+                    # letters from s2 only
+                    if s2[i2-1] == s3[i1 + i2 - 1] and dp[i1][i2-1]:
+                        dp[i1][i2] = True
+                    else:
+                        dp[i1][i2] = False
+                elif i2 == 0:
+                    if s1[i1-1] == s3[i1 + i2 - 1] and dp[i1-1][i2]:
+                        dp[i1][i2] = True
+                    else:
+                        dp[i1][i2] = False
+                else:
+                    if (s1[i1-1] == s3[i1 + i2 - 1] and dp[i1-1][i2]) or (s2[i2-1] == s3[i1 + i2 - 1] and dp[i1][i2-1]):
+                        dp[i1][i2] = True
+                    else:
+                        dp[i1][i2] = False
+        return dp[n1][n2]
 
-        def rec(i1, i2):
-            # i1, i2 - indecis of s1 and s2
-            # i3 = i1 + i2 - index s3
-            if i1 == len(s1) and i2 == len(s2):
-                return True
-            if (i1, i2) in memo:
-                return memo[(i1, i2)]
 
-            if i1 < len(s1) and s1[i1] == s3[i1 + i2] and rec(i1 + 1, i2):
-                memo[(i1, i2)] = True
-                return True
 
-            if i2 < len(s2) and s2[i2] == s3[i1 + i2] and rec(i1, i2 + 1):
-                memo[(i1, i2)] = True
-                return True
 
-            memo[(i1, i2)] = False
-            return False
 
         
-        return rec(0, 0)
             
 
-
+        
         
                     
             
