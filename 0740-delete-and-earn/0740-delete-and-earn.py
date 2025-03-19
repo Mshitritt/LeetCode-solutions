8d0281d55c756e19c54d82f577ffa0a9e04906d1
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-19 11:24:37 +0200

    Time: 5115 ms (5.03%), Space: 19.7 MB (59.09%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,66 +1,23 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-      
-        maxNum = max(nums)
         freq = Counter(nums)
-        if len(freq) == 1:
-            return nums[0]*freq[nums[0]]
-        dp = [-1]*(maxNum+1)
-        dp[0] = 0
-        self.res = 0
-        def rec(i):
-            if i > maxNum:
-                return 0
-            while i not in freq and i <= maxNum:
-                i += 1
-            a = -1
-            b = -1
-            if i + 1 < maxNum and dp[i+1] != -1:
-                a = dp[i+1]
-            else:
-                a = rec(i+1)
-            if i + 2 < maxNum and dp[i+2] != -1:
-                b = dp[i+2]
-            else:
-                b = rec(i+2)
+        nums = list(freq.keys())
 
-            c = (freq[i] * i)
-            dp[i] = max(a, b+c)
-            self.res = max(self.res, dp[i])
-            return dp[i]
-        
-        for i in range(1, maxNum):
-            dp[i] = rec(i)
-        return self.res
-        
-        """
-        if not nums:
-            return 0
+        nums.sort()           
+        dp = [0] * len(nums)
+        res = 0
+        for i in range(len(nums)):
+            num = nums[i]
+            curr = num * freq[num]
+            dp[i] = curr
+            for j in range(i):
+                if nums[j] + 1 != num:
+                    dp[i] = max(dp[i], dp[j] + curr)
+            res = max(res, dp[i])
+        return res
 
-        # Find the maximum value in nums to set up our dp array
-        max_num = max(nums)
-
-        # Create a frequency map to calculate total points for each number
-        points = [0] * (max_num + 1)
-        for num in nums:
-            points[num] += num
-
-        # Use a dp array to solve the problem similar to the "House Robber Problem"
-        dp = [0] * (max_num + 1)
-        dp[0] = 0
-        dp[1] = points[1]
-
-        # Fill the dp array using the transition relation
-        for i in range(2, max_num + 1):
-            dp[i] = max(dp[i - 1], dp[i - 2] + points[i])
-
-        # The answer is the maximum points we can earn
-        return dp[max_num]
-        """
-                
+            
 
 
-
-        
 
 
