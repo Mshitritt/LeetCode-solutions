cbf24654ffe1caa1886bad347b3fd36e3338a0c9
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-19 11:35:52 +0200

    Time: 4 ms (91.81%), Space: 19.4 MB (75.53%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,20 +1,36 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
+        
+                
         freq = Counter(nums)
         nums = list(freq.keys())
+        if len(nums) == 1:
+            return nums[0] * freq[nums[0]]
 
         nums.sort()           
         dp = [0] * len(nums)
-        res = 0
-        for i in range(len(nums)):
+        dp[0] = nums[0] * freq[nums[0]]
+        dp[1] = nums[1] * freq[nums[1]]
+        if nums[0] + 1 != nums[1]:
+            dp[1] += dp[0]
+        else:
+            dp[1] = max(dp[0], dp[1])
+
+        for i in range(2, len(nums)):
             num = nums[i]
             curr = num * freq[num]
-            dp[i] = curr
-            for j in range(i):
-                if nums[j] + 1 != num:
-                    dp[i] = max(dp[i], dp[j] + curr)
-            res = max(res, dp[i])
-        return res
+            prev_1 = nums[i-1]
+            prev_2 = nums[i-2]
+
+            if prev_1 + 1 != num:
+                dp[i] = curr + dp[i-1]
+            else:
+                dp[i] = max(
+                    curr + dp[i-2], dp[i-1]
+                )
+        return dp[-1]
+        
+            
 
             
 
