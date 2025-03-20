f1a5c7b3219a8dd0e38ffd313aa7bcb4b06cc9cc
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-20 10:24:09 +0200

    Time: 483 ms (29.16%), Space: 63 MB (25.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        """
         dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins))]
         # dp[c][amount] = number of ways to get amount by first c coins
         
@@ -8,20 +7,18 @@ class Solution:
         for i in range(len(coins)):
             dp[i][0] = 1
         
-        for c in range(len(coins)):
-            coin = coins[c]
-            for a in range(1, amount+1):
+        for a in range(1, amount+1):
+            
+            for c in range(len(coins)):
+                coin = coins[c]
                 if c == 0:
                     dp[c][a] = dp[c][a-coin] if a >= coin else 0
                 else:
                     dp[c][a] = dp[c-1][a]
                     dp[c][a] += dp[c][a-coin] if a >= coin else 0
         
+
         return dp[len(coins)-1][amount]
-        """
-        dp = [0]*(amount + 1)
-        # dp[i] = combinations to get amount i
-        dp[0] = 1
         """
         # all premetuations
         for a in range(1, amount + 1):
@@ -29,9 +26,6 @@ class Solution:
                 dp[a] += dp[a - coin] if a >= coin else 0
         return dp[amount]
         """
-        for coin in coins:
-            for a in range(1, amount + 1):
-                dp[a] += dp[a - coin] if a >= coin else 0
-        return dp[amount]
+        
 
 
