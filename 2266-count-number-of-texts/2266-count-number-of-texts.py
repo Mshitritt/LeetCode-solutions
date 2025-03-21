f0ccf07e196547a7b4a98aa2d416358e8638f8a1
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-21 13:16:28 +0200

    Time: 523 ms (47.44%), Space: 23.1 MB (58.76%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.py
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -1,26 +1,23 @@
 class Solution:
     def countTexts(self, pressedKeys: str) -> int:
-        s = pressedKeys
         MOD = 10**9 + 7
-        res = 0
-        dp = [0] * (len(s)+1)
+        n = len(pressedKeys)
+        s = pressedKeys
+        dp = [0] * (n + 1)
+        # dp[i] = number of text for s[i-1]
         dp[0], dp[1] = 1, 1
 
-        for i in range(2, len(s)+1):
-            dp[i] = dp[i - 1]  # Initialize dp[i] with the value from the previous key
+        for i in range(2, n + 1):
+            curr = s[i-1]
+            press = 4 if curr in "79" else 3
 
-            curr = s[i - 1]
-            max_presses = 4 if curr in "79" else 3  # Max presses depend on the key
-
-            for j in range(2, max_presses + 1):  # Start from 2 because 1 is already handled
-                if i - j >= 0 and s[i - j] == curr:
-                    dp[i] += dp[i - j]
-                    dp[i] %= MOD  # Apply modulo here to avoid overflow
+            for j in range(i-1, max(-1, i-press-1), -1):
+                if s[j] == curr:
+                    dp[i] += dp[j] % MOD
                 else:
-                    break  # Stop if keys do not match
+                    dp[i] += dp[i-1] if not dp[i] else 0
+                    break
+        return dp[n] % MOD
 
 
-        return dp[len(s)] % MOD
-      
-
-
+        
