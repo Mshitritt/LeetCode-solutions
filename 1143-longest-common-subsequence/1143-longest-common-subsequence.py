539bbb01bd0476fc505feca1abe4be06dfd890b4
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-03-26 14:26:52 +0200

    Time: 406 ms (87.84%), Space: 18 MB (91.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,22 +1,47 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Memoization 
-        dp = {}
+        # Bottom Up - Optimized space - VER 2
         n1, n2 = len(text1), len(text2)
-        def rec(i, j):
-            if (i, j) in dp:
-                return dp[(i, j)]
-            if i == n1 or j == n2:
-                    return 0
+        if n1 > n2:
+            text1, text2 = text2, text1
+            n1, n2 = n2, n1
+
+        # text1 - the short one 
+        dp = [0] * (n1 + 1)
+        
+        for j in range(1, len(text2) + 1):
+            prev = 0
+            for i in range(1, len(text1) + 1):
+                temp = dp[i]
+                if text1[i-1] == text2[j-1]:
+                    dp[i] = 1 + prev
+                else:
+                    dp[i] = max(dp[i-1], dp[i])
+                prev = temp
             
-            if text1[i] == text2[j]:
-                dp[(i, j)] = 1 + rec(i+1, j+1)
-            else:
-                dp[(i, j)] = max(rec(i+1, j), rec(i, j+1))
-            return dp[(i, j)]
         
+        return dp[len(text1)]
+        """
+        # Bottom Up - Optimized space 
+        n1, n2 = len(text1), len(text2)
+        if n2 < n1:
+            text1, text2 = text2, text1
+        # text1 - the short one 
+        prev = [0]*(len(text1) + 1)
+        curr = [0]*(len(text1) + 1)
+
+        for j in range(1, len(text2) + 1):
+            for i in range(1, len(text1) + 1):
+            
+                if text1[i-1] == text2[j-1]:
+                    curr[i] = 1 + curr[i-1]
+                else:
+                    curr[i] = max(curr[i-1], prev[i])
+            #curr, prev, = prev, curr
+            prev = curr.copy()
         
-        return rec(0, 0)
+        return prev[len(text1)]
+        """
                 
 
         
