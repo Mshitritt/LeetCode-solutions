3bf8044ede32bda65509dc29bffb61ff97a0b191
Matan Shitrit <90529221+Mshitritt@users.noreply.github.com>
2025-04-30 11:27:32 +0300

    Time: 10 ms (41.84%), Space: 18.2 MB (54.64%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,33 +1,26 @@
-class Solution(object):
-    def canPlaceFlowers(self, flowerbed, n):
-        """
-        :type flowerbed: List[int]
-        :type n: int
-        :rtype: bool
-        """
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        if len(flowerbed) == 1:
+            if n == 1 and flowerbed[0] == 0:
+                return True
+            return False
         i = 0
-        c = 0   # maximum available places 
-        n_flowerbed = len(flowerbed)    # size of flowers array 
-
-        while i < n_flowerbed:
-            if i+1 < n_flowerbed:
-                if flowerbed[i] == 0 and flowerbed[i+1] == 0:
-                    c += 1
-                    i += 2
-                elif flowerbed[i] == 1:
+        while i < len(flowerbed):
+            if i == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                n -= 1
+                flowerbed[i] = 0
+                i += 2
+            elif i == len(flowerbed)-1 and flowerbed[i] == 0 and flowerbed[i-1] == 0:
+                n -= 1
+                i += 2
+            else:
+                if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                    n -= 1
+                    flowerbed[i] = 1
                     i += 2
                 else:
                     i += 1
-            else:
-                if flowerbed[i-1] == 0 and flowerbed[i] == 0:
-                    c += 1
-                    break
-                else:
-                    break
-
-        if n <= c:
-            return True
-        else:
-            return False
-
+        return n <= 0
         
